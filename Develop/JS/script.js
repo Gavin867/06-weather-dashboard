9738b94a41bb680afc388d8463b91ff4112f6e74
Gavin Calkins <gavin.calkins@gmail.com>
2020-11-18 21:35:04 -0800

    added create btn

--- Develop/JS/script.js
+++ Develop/JS/script.js
@@ -2,10 +2,10 @@ var apiKey = "c91773251f63db01df3cd6ca70045ea5";
 
 var todaysDate = moment().format("MM/DD/YYYY");
 
-function retrieveWeather(citySearched) {
+function retrieveWeather(city) {
     console.log("Button Click")
 
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearched + "&appid=" + apiKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
     console.log(queryURL);
 
@@ -44,10 +44,21 @@ $("#searchBtn").on("click", function () {
 
     var citySearched = $("#searchCity").val();
 
+    var createBtn = $(`<button class="list-group-item">${citySearched}</button>`);
+
+    $("#btnContainer").prepend(createBtn);
+
     retrieveWeather(citySearched);
 });
 
+// $("#cities-list").on("click", "button", function () {
 
+//    $(this)
+
+//     var citySearched = $("#searchCity").val();
+
+//     retrieveWeather(citySearched);
+// });
 
 // City name is prepended to <ul>
 
