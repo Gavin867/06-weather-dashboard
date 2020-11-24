72a04d97bd4b03f5bbf32a063700bf618c4eebf5
Gavin Calkins <gavin.calkins@gmail.com>
2020-11-24 00:46:14 -0800

    reverted back to orginal API call

--- Develop/JS/script.js
+++ Develop/JS/script.js
@@ -14,7 +14,7 @@ function retrieveWeather(city) {
     
     console.log("Button Click");
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
     console.log(queryURL);
 
@@ -36,7 +36,7 @@ function retrieveWeather(city) {
 
             $("#currentDay").text("("+ currentDate + ")");
 
-            $("#mainIcon").attr("src", "https://openweathermap.org/img/wn/" + (results.weather[0].icon) + "@2x.png");
+            $("#mainIcon").attr("src", "http://openweathermap.org/img/wn/" + (results.weather[0].icon) + "@2x.png");
 
             $("#currentCityTemp").text("Temperature: " + results.main.temp + "° F");
 
@@ -70,7 +70,7 @@ function retrieveUVfiveday(lattitude, longitude) {
 
                 $("#forcastDate" + i).text(getDate(oneCallResults.daily[i].dt));
 
-                $("#forcastIcon" + i).attr("src", "https://openweathermap.org/img/wn/" + (oneCallResults.daily[i].weather[0].icon) + "@2x.png");
+                $("#forcastIcon" + i).attr("src", "http://openweathermap.org/img/wn/" + (oneCallResults.daily[i].weather[0].icon) + "@2x.png");
 
                 $("#forcastTemp" + i).text("Temp: " + oneCallResults.daily[i].temp.day + "° F");
 
